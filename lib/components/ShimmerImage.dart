f6936ec4e2514e50740107f28efc42b3d35f29c3
Dominik Dancs <dodancs@moow.info>
2020-03-21 11:41:02 +0100

    Obrazok s animaciou nacitania

---
+++ lib/components/ShimmerImage.dart
@@ -0,0 +1,42 @@
+import 'package:CiliCat/settings.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:CiliCat/providers/AuthProvider.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerImage extends StatelessWidget {
+  String picture;
+  double width, height;
+
+  ShimmerImage({this.picture, this.width, this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    return CachedNetworkImage(
+      imageUrl: Uri.http(API_URL, '/pictures/$picture').toString(),
+      httpHeaders: {
+        'Authorization':
+            authProvider.getTokenType + ' ' + authProvider.getToken,
+      },
+      fit: BoxFit.cover,
+      width: width,
+      height: height,
+      placeholder: (context, url) => SizedBox(
+        width: width,
+        height: height,
+        child: Shimmer.fromColors(
+          baseColor: Colors.grey[300],
+          highlightColor: Colors.grey[100],
+          child: Container(
+            width: width,
+            height: height,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      errorWidget: (context, url, error) => Icon(Icons.error),
+    );
+  }
+}
